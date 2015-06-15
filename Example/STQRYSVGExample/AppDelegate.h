d1edd533b1dea0edba3e70478834ff956ec8c7d8
Jake <jake.bellamy@stqry.com>
2015-06-15 20:06:11 +1200

    Setup example

---
+++ Example/STQRYSVGExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STQRYSVGExample
+//
+//  Created by Jake Bellamy on 15/06/15.
+//  Copyright (c) 2015 STQRY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/STQRYSVGExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STQRYSVGExample
+//
+//  Created by Jake Bellamy on 15/06/15.
+//  Copyright (c) 2015 STQRY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
