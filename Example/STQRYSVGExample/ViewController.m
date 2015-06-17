a3d50ebd8305d3b5a762a1d40b1f584f14ed0712
Jake <jake.bellamy@stqry.com>
2015-06-17 15:41:17 +1200

    Better example

--- Example/STQRYSVGExample/ViewController.m
+++ Example/STQRYSVGExample/ViewController.m
@@ -11,18 +11,30 @@
 
 @interface ViewController ()
 
-@property (nonatomic, copy) NSArray *svgAssetNames;
+@property (nonatomic, copy, readonly) NSArray *svgAssetNames;
+
 @property (nonatomic, weak) IBOutlet UIImageView *imageView;
+@property (nonatomic, weak) IBOutlet UIWebView *webView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
+- (NSArray *)svgAssetNames
 {
-    [super viewDidLoad];
-    self.svgAssetNames = @[@"Happyface", @"fb", @"appleLogo", @"star", @"male", @"atom", @"close", @"location", @"marker", @"recent", @"search", @"walking", @"wheelchair", @"polyline"];
-    self.imageView.svgImageName = [self.svgAssetNames firstObject];
+    return @[@"Happyface", @"fb", @"appleLogo", @"star", @"male", @"atom", @"close", @"location", @"marker", @"recent", @"search", @"walking", @"wheelchair", @"polyline"];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self displaySVGAtIndex:0];
+}
+
+- (void)viewDidLayoutSubviews
+{
+    [super viewDidLayoutSubviews];
+    self.imageView.svgImageSize = self.imageView.bounds.size;
 }
 
 - (IBAction)nextBarButtonPressed:(UIBarButtonItem *)sender
@@ -32,7 +44,21 @@
     // Wrap back to 0 once end of array has been reached.
     NSUInteger nextIndex = currentIndex + 1 >= self.svgAssetNames.count ? 0 : currentIndex + 1;
     
-    self.imageView.svgImageName = self.svgAssetNames[nextIndex];
+    [self displaySVGAtIndex:nextIndex];
 }
+
+- (void)displaySVGAtIndex:(NSUInteger)index
+{
+    NSString *svgName = self.svgAssetNames[index];
+    
+    // Image view
+    self.imageView.svgImageName = svgName;
+    
+    // Web view
+    NSURL *url = [[NSBundle mainBundle] URLForResource:svgName withExtension:@"svg"];
+    NSString *svgString = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:nil];
+    [self.webView loadHTMLString:svgString baseURL:nil];
+}
+
 
 @end
