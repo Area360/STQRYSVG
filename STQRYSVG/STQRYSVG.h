a12cbae1af4b85c7f1ab75a01830491f20d521df
Daniel Clelland <daniel.clelland@stqry.com>
2015-06-19 12:24:30 +1200

    Added STQRYSVG.h

---
+++ STQRYSVG/STQRYSVG.h
@@ -0,0 +1,11 @@
+//
+//  STQRYSVG.h
+//  STQRYSVG
+//
+//  Created by Daniel Clelland on 15/06/15.
+//  Copyright (c) 2015 STQRY. All rights reserved.
+//
+
+#import "UIImage+STQRYSVG.h"
+#import "UIImageView+STQRYSVG.h"
+#import "UIButton+STQRYSVG.h"
